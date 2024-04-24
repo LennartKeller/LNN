8a1fca9e9e6eca6c0736da6719a47c4a063c1516
Lennart Keller <lennartkeller@gmail.com>
2024-04-24 14:19:39 +0200

    update sguardian

--- src/lnn/slurm.py
+++ src/lnn/slurm.py
@@ -3,6 +3,7 @@ import os
 import shlex
 import subprocess
 import time
+from datetime import datetime
 from pathlib import Path
 from typing import Optional
 
@@ -15,6 +16,38 @@ SBATCH_DEFAULT_ARGS = [
     "--cpus-per-task=64",
     "--mem=64gb",
 ]
+
+SACCT_TIMESTAMP_FORMAT = "%Y-%m-%d"
+
+
+def job_is_finished(
+    job_id: Optional[int] = None, job_name: Optional[str] = None
+) -> bool:
+    now = datetime.now()
+    last_month = now.replace(month=now.month - 1)
+    start_timestamp = last_month.strftime(SACCT_TIMESTAMP_FORMAT)
+    end_timestamp = last_month.strftime(SACCT_TIMESTAMP_FORMAT)
+    sacct_args = ["sacct", "-P", "-S", start_timestamp, "-E", end_timestamp]
+    sacct_output = subprocess.run(
+        [" ".join(sacct_args)], shell=True, capture_output=True, check=True
+    ).stdout.decode("utf-8")
+    joblist = sacct_output.split("\n")[1:]
+    for job in joblist:
+        if not job.strip():
+            continue
+        entries = job.split("|")
+        id = entries[0]
+        name = entries[1]
+        state = entries[-2]
+        id = id.split(".")[0]
+        if job_id is not None:
+            if str(job_id) == id and state.strip() == "COMPLETED":
+                return True
+        else:
+            assert job_name is not None, "Provide either job_name or job_id"
+            if name.strip().lower() == job_name.strip().lower():
+                return True
+    return False
 
 
 def get_jobs() -> dict[str, str | int]:
@@ -87,6 +120,14 @@ def slurm_guardian(watch_config: list[dict], every: int = 30):
                     )
                     break
             if not is_scheduled:
+                is_already_finished = job_is_finished(job_name=job_name)
+                force = entry.get("force", False)
+                if is_already_finished and not force:
+                    print(
+                        f"Job '{job_name}'is not scheduled but seems to be finished..."
+                        " Skipping resubmission! Set force=True in watch config to force resubmission."
+                    )
+                    continue
                 print(f"Did not found job '{job_name}'! Scheduling it now.")
                 sbatch_prc = sbatch(**entry["sbatch_kwargs"], verbose=True)
                 stdout, stderr = sbatch_prc.communicate()
@@ -111,9 +152,9 @@ def slurm_guardian(watch_config: list[dict], every: int = 30):
 def sguardian(watch_config, every):
     """Watches over slurm jobs every N seconds.
 
-    If the job cannot be found, it es (re-)submitted.
+    If the job cannot be found, it is (re-)submitted.
 
-    Specify job name and how to submit the job in a watch-config JSON file:
+    Specify jobs to watch and how to submit them via a watch-config JSON file:
 
     \b
     [
@@ -136,7 +177,8 @@ def sguardian(watch_config, every):
         },
         ...
     ]
-
+    Usage:
+        $ sguardian --every 20 my-watch-config.json
     """
     watch_config = json.loads(Path(watch_config).read_text())
     slurm_guardian(watch_config=watch_config, every=every)
