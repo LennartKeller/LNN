de93a9ffe49b42735ff15a951cc3599e99cfc550
Lennart Keller <lennartkeller@gmail.com>
2024-06-07 14:13:30 +0200

    revert argument handling in dict helpers all together

--- src/lnn/utils.py
+++ src/lnn/utils.py
@@ -54,11 +54,11 @@ def get_lang_code(language: str) -> str:
     return None
 
 
-def extracts(d: dict, *keys: list) -> dict:
+def extracts(d: dict, keys: list) -> dict:
     return {k: d[k] for k in keys}
 
 
-def gets(d: dict, *keys: list) -> list:
+def gets(d: dict, keys: list) -> list:
     return [d[k] for k in keys]
 
 
