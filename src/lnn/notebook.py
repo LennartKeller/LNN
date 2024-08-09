61a54793e4a490ac73e5c00f163e42e5e91015ca
Lennart Keller <lennartkeller@gmail.com>
2024-08-09 12:48:41 +0200

    max waveform arg optional

--- src/lnn/notebook.py
+++ src/lnn/notebook.py
@@ -9,11 +9,15 @@ from .utils import load_audio
 
 
 def display_audio(
-    waveform: torch.Tensor | np.ndarray,
+    waveform: Optional[torch.Tensor | np.ndarray] = None,
     rate: Optional[int] = None,
     path: Optional[str | Path] = None,
 ):
     if path is None:
+        if waveform is None:
+            raise ValueError(
+                "Audio has to either provided as waveform or via a path to an audio-file."
+            )
         if rate is None:
             raise ValueError(
                 "If a waveform is provided, the rate must also be specified."
