9d91da06e81bd04d52caf9cee9eda48fc19f5844
Lennart Keller <lennart.keller@googlemail.com>
2024-08-25 08:47:41 +0200

    add toplevel imports for utils

--- src/lnn/__init__.py
+++ src/lnn/__init__.py
@@ -1,3 +1,25 @@
+from lnn.notebook import display_audio
+
 from .attr_dict import AttrDict, TensorAttrDict
 from .base import BaseConfig, ConfigurableLightningModule
-from .utils import count_params, normalize_lang_id
+from .pathlib import Path
+from .utils import (
+    conv1d_output_length,
+    count_params,
+    dataframe_map,
+    dataset_dict_to_ndjson,
+    dataset_dict_to_tsv,
+    extracts,
+    flatten_dataset_dict,
+    freeze_module,
+    get_conv1d_mod_output_length,
+    get_lang_code,
+    gets,
+    load_audio,
+    load_dataset_split_from_disk,
+    load_ndjson_dataset,
+    load_tsv_dataset,
+    normalize_lang_id,
+    pad_waveform,
+    rename_keys,
+)
