ac4f311e5ce6967d2d54e603d2d480e525cb0a43
Lennart Keller <lennart.keller@googlemail.com>
2024-12-09 14:27:35 +0100

    add flag for recursive merging

--- src/lnn/codebase.py
+++ src/lnn/codebase.py
@@ -2,12 +2,15 @@ from pathlib import Path
 import click
 
 
-def merge_codebase(root_dir: str | Path, include_suffixes: tuple[str] = ("py",)) -> str:
+def merge_codebase(
+    root_dir: str | Path, include_suffixes: tuple[str] = ("py",), recursive: bool = True
+) -> str:
     """Merge all code files in the given directory into a single string."""
     cwd = Path().cwd()
     root_dir = Path(root_dir)
     code = ""
-    for path in root_dir.glob("**/*"):
+    pattern = "**/*" if recursive else "*"
+    for path in root_dir.glob(pattern):
         path = path.resolve()
         if path.is_file() and path.suffix[1:] in include_suffixes:
             name = path.relative_to(cwd)
@@ -24,7 +27,8 @@ def merge_codebase(root_dir: str | Path, include_suffixes: tuple[str] = ("py",))
     multiple=True,
     help="File suffixes to include in the merge.",
 )
-def cli_main(root_dir: str, include_suffix: tuple[str]):
+@click.option("--recursive", "-r", is_flag=True, help="Recursively search for files.")
+def cli_main(root_dir: str, include_suffix: tuple[str], recursive: bool) -> None:
     """Merge all code files in the given directory into a single string."""
-    code = merge_codebase(root_dir, include_suffix)
+    code = merge_codebase(root_dir, include_suffix, recursive)
     print(code)
